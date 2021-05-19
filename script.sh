8504b5f6a5a364e672712b469850810d84887ffc
mostafafawzy <mostafafawzy@users.noreply.github.com>
2021-05-19 19:32:59 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,5 +2,5 @@
 echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
-echo "Hello Mostafa Fawzy Mahmoud Salem mostafa"
+echo "Hello Mostafa Fawzy Mahmoud Salem 1"
 echo "Success"
